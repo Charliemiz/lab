7ed978234245d2aad2597ae49220a7be66c656b2
Charliemiz <120235758+Charliemiz@users.noreply.github.com>
2023-04-18 17:08:32 -0500

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -10,16 +10,16 @@ def test_account_init():
 
 def test_account_deposit():
     account = Account("Jim")
-    assert account.deposit(100) is True
+    assert account.deposit(50) is True
     assert account.get_balance() == 100
-    assert account.deposit(-50) is False
+    assert account.deposit(-10) is False
     assert account.get_balance() == 100
 
 
 def test_account_withdraw():
     account = Account("Jim")
-    account.deposit(100)
+    account.deposit(70)
     assert account.withdraw(50) is True
-    assert account.get_balance() == 50
+    assert account.get_balance() == 20
     assert account.withdraw(100) is False
-    assert account.get_balance() == 50
+    assert account.get_balance() == 20
